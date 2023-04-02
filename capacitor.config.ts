8c605854b4594877ef2cd3ca2bedf41b7cafafa7
Sh3lby007 <zhengshaobin@ymail.com>
2023-04-02 19:16:12 +0800

    Add capacitorto support android + ios.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'qifeng.com',
+  appName: 'qifeng',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
